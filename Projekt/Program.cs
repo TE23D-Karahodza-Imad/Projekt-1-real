1db713891edfdb81013c43a6072baf10acfac035
TE23D-Karahodza-Imad <imadahmed.karahodza@elev.ga.ntig.se>
2025-01-31 10:08:14 +0100

    Hello

---
+++ Projekt/Program.cs
@@ -0,0 +1,27 @@
+﻿//Jag vill skaffa dagar.
+
+System.Console.WriteLine("Hello, welcome to my crazy game of adventures.. says Simon");
+System.Console.WriteLine("But who is Simon, he is a Gnome!");
+System.Console.WriteLine("The crazy Gnome of the Red Forest");
+Console.WriteLine("Press any key to continue..");
+Console.ReadLine();
+
+System.Console.WriteLine("Gnome: Do you want to be a Bowman or a Swordsman?");
+
+System.Console.WriteLine("Gnome: Here take these items!");
+string[] bowmanItems = [
+    "Epirus(Bow)",
+    "Arrow Quiver",
+    "Medium Apple-Sack"
+];
+Console.WriteLine(bowmanItems);
+Console.ReadLine();
+
+System.Console.WriteLine("Gnome: Here take these items!");
+string[] swordsmanItems = [
+    "Excalibur(Sword)",
+    ""
+];
+Console.WriteLine(swordsmanItems);
+Console.ReadLine();
+
